0b84751963902a554404c2a3f347637fc7c62c4f
Dwinanda <dwinanda.wicaksana@student.umn.ac.id>
2021-10-19 12:44:25 +0700

    tambah nav menu

--- app/src/main/java/com/contoh/reresik/MainActivity.java
+++ app/src/main/java/com/contoh/reresik/MainActivity.java
@@ -1,12 +1,21 @@
 package com.contoh.reresik;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +23,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         FragmentHome frag1 = new FragmentHome();
         frag1.setArguments(getIntent().getExtras());
 
@@ -24,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
         fragTransaction.add(R.id.frame_1,frag1);
         fragTransaction.commit();
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
